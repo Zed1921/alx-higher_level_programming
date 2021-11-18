31af4e4b22fa14c0e9fa13ca36aad9a858cf1613
zed1921 <zewge1921@gmail.com>
2021-11-18 08:03:57 -0800

    file_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
 
-def add(a, b):
-        a = 1
-        b = 2
-        sum = a + b
-        print("{} + {} = {}".format(a, b, sum))
 
-if __name__ == "__add__":
-    add()
+def main():
+            a = 1
+            b = 2
+            c = add(a, b)
+            print("{} + {} = {}".format(a, b, c))
+
+if __name__ == "__main__":
+        main()
