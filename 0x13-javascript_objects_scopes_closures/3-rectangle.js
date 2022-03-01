da2765b317c00e596b2bd438e77a34368033fecf
zed1921 <zewge1921@gmail.com>
2022-03-01 10:39:25 -0800

    javascript_objects_scopes_closures project

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,11 +4,11 @@ class Rectangle {
     if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
       this.width = w;
       this.height = h;
-	}
     }
+  }
 
   print () {
     console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
-    }
+  }
 }
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,8 +4,8 @@ class Rectangle {
     if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
       this.width = w;
       this.height = h;
-	}
     }
+  }
 
   print () {
     console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
